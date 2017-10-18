c2ee301b725354b7edf8469c0cd2cfeeb7323783
William Reed <willie.b.reed@gmail.com>
2017-10-18 13:17:33 -0400

    enhanced make_links.sh

--- make_links.sh
+++ make_links.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 mkdir -p ~/.config  # check for existence of .config and create if it doesn't exist
+[[ -f ~/.config/nvim ]] && mv ~/.config/nvim ~/.config/nvim.old
 ln -s ~/.vim ~/.config/nvim 
